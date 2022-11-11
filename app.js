5084d511eb0fc1d17cf590d468ddd5dfe65af894
TTepel7 <55439124+TTepel7@users.noreply.github.com>
2022-11-11 10:24:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+function Notification(imt) {
+    const notifTitle = 'Калькулятор ИМТ';
+
+    const options = {
+        body: 'Ваш последний ИМТ, ' + imt,
+        icon: 'images/icon.png',
+    };
+    new Notification(notifTitle, options);
+    setTimeout(Notification, 30000);
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // получение элементов по их Id
     const height_elem = document.getElementById('height')
@@ -13,15 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
         let imt = w / (h * h)
         // запись ответа в поле ответа, с округлением до 2 знаков
         output_elem.value = imt.toFixed(2)
+
+        Notification.requestPermission().then((result) => {
+            if (result === 'granted') {
+                randomNotification();
+            }
+        })
+        // регистрация serviceWorker
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js')
+                .then(registration => {
+                    console.log('SW registred', registration)
+                })
+                .catch(error => {
+                    console.log('SW failed', error)
+                })
+
+        }
     })
-    // регистрация serviceWorker
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js')
-            .then(registration => {
-                console.log('SW registred', registration)
-            })
-            .catch(error => {
-                console.log('SW failed', error)
-            })
-    }
-})
